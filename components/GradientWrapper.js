81ade3b4acb98cfed538fa3cbe2e38096790037d
thornus <luca@DESKTOP-QPQSMU2.localdomain>
2020-04-16 02:06:51 -0400

    Use test ad for iOS

--- components/GradientWrapper.js
+++ components/GradientWrapper.js
@@ -5,7 +5,7 @@ import { AdMobBanner } from 'expo-ads-admob';
 import design from '../utils/design';
 
 const iphoneTestId = 'E55E2963-962A-42A2-9CF7-1D1BB80387A3';
-const adUnitID = Platform.OS === 'ios' ? "ca-app-pub-3940256099942544/3986624511" : "ca-app-pub-3940256099942544/6300978111"; //"ca-app-pub-1264194119076568/4378676945"
+const adUnitID = Platform.OS === 'ios' ? "ca-app-pub-3940256099942544/2934735716" : "ca-app-pub-3940256099942544/6300978111"; //"ca-app-pub-1264194119076568/4378676945"
 
 export default GradientWrapper = ({viewExtendedStyle, style, children, noKeyboardDismiss, adPosition = 'top'}) => {
   const adStyleBottom = adPosition === 'bottom' && {flexGrow: 1};
